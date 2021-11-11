7ea2655ea425c5d6263487a565e9c4f2f3fb48da
BPugna <bapugnaloni98@gmail.com>
2021-11-11 15:59:45 -0300

    comment deleted

--- api/src/routes/countries.js
+++ api/src/routes/countries.js
@@ -6,13 +6,8 @@ require('dotenv').config()
 const { Op } = require('sequelize');
 
 
-    // TODO [ ] GET /countries?name="...":
-// Obtener los países que coincidan con el nombre pasado como query parameter (No necesariamente tiene que ser una matcheo exacto)
-// Si no existe ningún país mostrar un mensaje adecuado
-
 
 router.get('/', async (req, res, next)=>{
-    
     const { name } = req.query;
     if(name){
         const match = await Country.findAll({ 
@@ -24,57 +19,45 @@ router.get('/', async (req, res, next)=>{
         })
         if(match !== 0){
             return res.send(match)
-        } else {
-            return res.status(404).send("No se encontro el pais buscado");
         }
     }
     next()
 })
     
-/*
-TODO [ ] GET /countries:
-    En una primera instancia deberán traer todos los países desde restcountries y guardarlos en su propia base de datos y luego ya utilizarlos desde allí (Debe almacenar solo los datos necesarios para la ruta principal)
-    Obtener un listado de los paises.
-*/
    
-   
-   router.get('/', async (req,res) => {
-       let DB = await Country.findAll();
-       if(!DB.length){
-            var apiRequest = await axios.get(`https://restcountries.com/v3/all`)
-            .catch(err => {
-                return res.status(404).send(err);
-             });
 
-                apiRequest.data.map(country => {
-                Country.findOrCreate({
-                    where : {
-                        id: country.cca3,
-                    },
-                    defaults: {
-                        id: country.cca3,
-                        name: country.name.common,
-                        flag: country.flags[1],
-                        region: country.region,
-                        capital: country.capital?.[0],
-                        subregion: country.subregion,
-                        area: country.area,
-                        population: country.population,
-                    }
-                    });
-            })
-        };
-        let elAwait = await Country.findAll({
-            include: Activity
-        }) 
-        return res.status(201).send(elAwait)
+router.get('/', async (req,res) => {
+    let DB = await Country.findAll();
+    if(!DB.length){
+        var apiRequest = await axios.get(`https://restcountries.com/v3/all`)
+        .catch(err => {
+            return res.status(404).send(err);
+            });
+
+            apiRequest.data.map(country => {
+            Country.findOrCreate({
+                where : {
+                    id: country.cca3,
+                },
+                defaults: {
+                    id: country.cca3,
+                    name: country.name.common,
+                    flag: country.flags[1],
+                    region: country.region,
+                    capital: country.capital?.[0],
+                    subregion: country.subregion,
+                    area: country.area,
+                    population: country.population,
+                }
+                });
+        })
+    };
+    let elAwait = await Country.findAll({
+        include: Activity
+    }) 
+    return res.status(201).send(elAwait)
 })
 
-// TODO [ ] GET /countries/{idPais}:
-//     Obtener el detalle de un país en particular
-//     Debe traer solo los datos pedidos en la ruta de detalle de país
-//     Incluir los datos de las actividades turísticas correspondientes
-// TODO  ##### CAPAZ TENGA QUE AGREGAR UN EXCLUDE PARA DATOS QUE NO PIDEN #####
 
 router.get('/:id', async (req, res)=>{
     let { id } = req.params;
